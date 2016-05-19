d3f04dd2b16ea2ccf7416ff1d91f2ab4ac8af267
Mitsunori Komatsu <komamitsu@gmail.com>
2016-05-19 16:27:39 +0900

    Bump version to 3.2.28

--- KeenClient/KeenConstants.h
+++ KeenClient/KeenConstants.h
@@ -8,7 +8,7 @@
 
 #import <Foundation/Foundation.h>
 
-#define kKeenSdkVersion @"3.2.27"
+#define kKeenSdkVersion @"3.2.28"
 
 extern NSString * const kKeenServerAddress;
 extern NSString * const kKeenApiVersion;
