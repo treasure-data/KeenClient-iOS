c784132ba262009503e693e4a4b3711606c928e5
Guillermo Ignacio Enriquez Gutierrez <ignacio@jp.ibm.com>
2021-07-09 09:56:45 +0900

    Add SPM support

---
+++ Package.swift
@@ -0,0 +1,52 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "KeenClientTD",
+    platforms: [
+        .macOS(.v10_10),
+        .iOS(.v11),
+        .tvOS(.v9),
+        .watchOS(.v2)
+    ],
+    products: [
+        .library(
+            name: "KeenClientTD",
+            targets: ["KeenClientTD"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+      .target(
+            name: "KeenClientTD",
+            dependencies: [
+            ],
+            path: ".",
+            exclude: [
+                "KeenClient.xcodeproj/",
+                "KeenClientExample/",
+                "KeenClientTD.podspec",
+                "KeenClientTests",
+                "Library/Headers/OCMock/",
+                "Library/libOCMock.a",
+                "Makefile",
+                "README.md",
+                "docs",
+                "generate_docs.sh",
+            ],
+            sources: [
+                "KeenClient/",
+                "KeenClientInternal/",
+                "Library/sqlite-amalgamation/",
+            ],
+            publicHeadersPath: "KeenClient",
+            cxxSettings: [
+                .headerSearchPath("Library/sqlite-amalgamation"),
+                .headerSearchPath("KeenClient"),
+                .headerSearchPath("KeenClientInternal"),
+            ]
+        ),
+    ],
+    cxxLanguageStandard: .cxx1z
+)
